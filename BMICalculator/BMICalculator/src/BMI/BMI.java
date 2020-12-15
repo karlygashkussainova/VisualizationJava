599a878274e1d30a71f119adcd75d190b9570a7d
karlygashkussainova <51878709+karlygashkussainova@users.noreply.github.com>
2020-12-15 13:14:52 +0600

    Add files via upload

---
+++ BMICalculator/BMICalculator/src/BMI/BMI.java
@@ -0,0 +1,25 @@
+package BMI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class BMI extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("BMI.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("BMI calculator");
+        stage.setScene(scene);
+        stage.show();}
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ BMICalculator/BMICalculator/src/BMI/BMIController.java
@@ -0,0 +1,99 @@
+package BMI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class BMIController {
+
+    @FXML
+    private Button metricsButton;
+
+    @FXML
+    private Button englishButton;
+
+    @FXML
+    private TextField weightMetricsTextField;
+
+    @FXML
+    private TextField heightMetricsTextField;
+
+    @FXML
+    private TextField weightEnglishTextField;
+
+    @FXML
+    private TextField heightEnglishTextField;
+
+    @FXML
+    private TextField bmiValueTextField;
+
+    @FXML
+    void onEnglishPressed(ActionEvent event) {
+        try{
+            double bmiIndex;
+            double weight = Double.parseDouble(weightEnglishTextField.getText());
+            double height = Double.parseDouble(heightEnglishTextField.getText());
+            bmiIndex = (703 * weight) / (height * height);
+
+            if (bmiIndex <= 18.5){
+                bmiValueTextField.setText("Underweight");
+            }
+            else if (bmiIndex <= 24.9){
+                bmiValueTextField.setText("Normal");
+            }
+            else if (bmiIndex <= 29.9){
+                bmiValueTextField.setText("Overweight");
+            }
+            else if (bmiIndex > 30.0){
+                bmiValueTextField.setText("Obese");
+            }
+
+        }
+        catch (NumberFormatException e){
+            weightEnglishTextField.setText("Enter weight");
+            weightEnglishTextField.selectAll();
+            weightEnglishTextField.requestFocus();
+
+            heightEnglishTextField.setText("Enter height");
+            heightEnglishTextField.selectAll();
+            heightEnglishTextField.requestFocus();
+        }
+
+    }
+
+    @FXML
+    void onMetricsPressed(ActionEvent event) {
+        try{
+            double bmiIndex;
+            double weight = Double.parseDouble(weightMetricsTextField.getText());
+            double height = Double.parseDouble(heightMetricsTextField.getText());
+            bmiIndex = (weight) / (height * height);
+
+            if (bmiIndex <= 18.5){
+                bmiValueTextField.setText("Underweight");
+            }
+            else if (bmiIndex <= 24.9){
+                bmiValueTextField.setText("Normal");
+            }
+            else if (bmiIndex <= 29.9){
+                bmiValueTextField.setText("Overweight");
+            }
+            else if (bmiIndex > 30.0){
+                bmiValueTextField.setText("Obese");
+            }
+
+        }
+        catch (NumberFormatException e){
+            weightMetricsTextField.setText("Enter weight");
+            weightMetricsTextField.selectAll();
+            weightMetricsTextField.requestFocus();
+
+            heightMetricsTextField.setText("Enter height");
+            heightMetricsTextField.selectAll();
+            heightMetricsTextField.requestFocus();
+        }
+
+    }
+
+}
