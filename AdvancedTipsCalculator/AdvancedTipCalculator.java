eb47b3aafafa921938827a9c2a37d78db622e740
karlygashkussainova <51878709+karlygashkussainova@users.noreply.github.com>
2020-12-23 00:39:23 +0600

    Add files via upload

---
+++ AdvancedTipsCalculator/AdvancedTipCalculator.java
@@ -0,0 +1,24 @@
+package AdvancedTipsCalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class AdvancedTipCalculator extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("AdvancedTipsCalculator.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Tips Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
